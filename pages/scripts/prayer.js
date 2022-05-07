5e4e64279b7e67baf96a1e9b7784f0fdc65f87c5
Usama <104801912+UsamaBased@users.noreply.github.com>
2022-05-07 12:07:14 +0000

    Added temple

---
+++ pages/scripts/prayer.js
@@ -0,0 +1,16 @@
+function pray() {
+  console.log("prayed")
+  let prayer = parseInt(localStorage.getItem("Prayers"));
+  
+  if (prayer == null) {
+    localStorage.setItem("Prayers", parseInt(0));
+  }
+  else {
+    localStorage.setItem("Prayers", prayer+1);
+  }
+  document.getElementById("prayer_count").innerHTML = `Prayers: ${localStorage.getItem("Prayers")}`;
+}
+
+function set_prayer() { 
+  document.getElementById("prayer_count").innerHTML = `Prayers: ${localStorage.getItem("Prayers")}`;
+}
